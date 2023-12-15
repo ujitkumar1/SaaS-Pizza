36242910bdddefe089f49ecfbd9e8defe68bee00
Ujit Kumar <ujitkumar1@gmail.com>
2023-12-16 00:30:53 +0530

    most post and expried post

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
+import warnings
 from datetime import datetime
 from json import JSONEncoder
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager, create_access_token
 from flask_jwt_simple import jwt_required, get_jwt_identity
 from flask_restful import Api, Resource, reqparse
 from flask_sqlalchemy import SQLAlchemy
+
+warnings.filterwarnings('ignore')
 
 
 class CustomJSONEncoder(JSONEncoder):
@@ -106,28 +109,23 @@ class UserLogin(Resource):
 
 
 class PostMessage(Resource):
-    @jwt_required  # Use the correct decorator from flask_jwt_simple
+    @jwt_required
     def post(self):
-        # Create a new parser for the post-related fields
         post_parser = reqparse.RequestParser()
         post_parser.add_argument('title', help='This field cannot be blank', required=True)
         post_parser.add_argument('topic', help='This field cannot be blank', required=True)
         post_parser.add_argument('body', help='This field cannot be blank', required=True)
         post_parser.add_argument('expiration_time', help='This field cannot be blank', required=True)
 
-        # Get the current user from the JWT token
         current_user = get_jwt_identity()
 
-        # Parse the request data using the post_parser
         data = post_parser.parse_args()
 
-        # Extract post details from data
         title = data['title']
         topic = data['topic']
         body = data['body']
         expiration_time = datetime.strptime(data['expiration_time'], '%Y-%m-%dT%H:%M:%S')
 
-        # Save post details to the database with the current_user as the owner
         new_post = Post(
             title=title,
             topic=topic,
@@ -145,10 +143,8 @@ class PostMessage(Resource):
 class BrowseMessages(Resource):
     @jwt_required
     def get(self, topic_name, post_id=None):
-        # Get the current user from the JWT token
         current_user = get_jwt_identity()
 
-        # Check if a specific post ID is provided
         if post_id is not None:
             post = Post.query.filter_by(id=post_id, topic=topic_name, status='Live').first()
             if not post:
@@ -161,18 +157,15 @@ class BrowseMessages(Resource):
                 'timestamp': post.timestamp.strftime('%Y-%m-%dT%H:%M:%S'),
                 'expiration_time': post.expiration_time.strftime('%Y-%m-%dT%H:%M:%S'),
                 'owner_name': post.owner_name,
-                'body': post.body,  # Include the post body in the response
+                'body': post.body,
                 'likes': post.likes,
                 'dislikes': post.dislikes
             }
 
-        # Retrieve messages related to the specified topic
         messages = Post.query.filter_by(topic=topic_name, status='Live').all()
 
-        # Create a list to store the messages
         result = []
 
-        # Iterate through the messages and create a response
         for message in messages:
             result.append({
                 'id': message.id,
@@ -210,16 +203,13 @@ class UserInteractions(Resource):
         interaction_type = data['interaction_type']
         time_left = data['time_left']
 
-        # Check if the interaction type is valid
         if interaction_type not in ['like', 'dislike', 'comment']:
             return {'message': 'Invalid interaction type'}, 400
 
-        # Check if the post exists
         post = Post.query.get(post_id)
         if not post:
             return {'message': 'Post not found'}, 404
 
-        # Perform the requested interaction
         if interaction_type == 'like':
             post.likes += 1
         elif interaction_type == 'dislike':
@@ -231,7 +221,6 @@ class UserInteractions(Resource):
             comment_data = comment_parser.parse_args()
             comment_text = comment_data['comment_text']
 
-            # Save the comment to the Comment model
             new_comment = Comment(
                 user_name=current_user,
                 text=comment_text,
@@ -239,7 +228,6 @@ class UserInteractions(Resource):
             )
             db.session.add(new_comment)
 
-        # Save the changes to the database
         db.session.commit()
 
         return {'message': f'{interaction_type.capitalize()} added successfully'}
@@ -248,18 +236,14 @@ class UserInteractions(Resource):
 class ViewComments(Resource):
     @jwt_required
     def get(self, post_id):
-        # Check if the post exists
         post = Post.query.filter_by(id=post_id, status='Live').first()
         if not post:
             return {'message': 'Post not found'}, 404
 
-        # Retrieve comments for the post
         comments = Comment.query.filter_by(post_id=post_id).all()
 
-        # Create a list to store the comments
         result = []
 
-        # Iterate through the comments and create a response
         for comment in comments:
             result.append({
                 'id': comment.id,
@@ -274,22 +258,18 @@ class ViewComments(Resource):
 class PostComment(Resource):
     @jwt_required
     def post(self, post_id):
-        # Get the current user from the JWT token
         current_user = get_jwt_identity()
 
-        # Parse the request data for posting comments
         comment_parser = reqparse.RequestParser()
         comment_parser.add_argument('comment_text', help='Comment text is required', required=True)
 
         comment_data = comment_parser.parse_args()
         comment_text = comment_data['comment_text']
 
-        # Check if the post exists
         post = Post.query.get(post_id)
         if not post:
             return {'message': 'Post not found'}, 404
 
-        # Save the comment to the Comment model
         new_comment = Comment(
             user_name=current_user,
             text=comment_text,
@@ -299,6 +279,58 @@ class PostComment(Resource):
         db.session.commit()
 
         return {'message': 'Comment added successfully'}
+
+
+class MostActivePosts(Resource):
+    @jwt_required
+    def get(self):
+        topics = db.session.query(Post.topic).distinct().all()
+
+        most_active_posts = {}
+
+        for topic in topics:
+            topic_name = topic[0]
+
+            most_active_post = Post.query.filter_by(topic=topic_name, status='Live') \
+                .order_by(Post.likes.desc(), Post.dislikes.desc()).first()
+
+            if most_active_post:
+                most_active_posts[topic_name] = {
+                    'id': most_active_post.id,
+                    'title': most_active_post.title,
+                    'topic': most_active_post.topic,
+                    'timestamp': most_active_post.timestamp.strftime('%Y-%m-%dT%H:%M:%S'),
+                    'expiration_time': most_active_post.expiration_time.strftime('%Y-%m-%dT%H:%M:%S'),
+                    'owner_name': most_active_post.owner_name,
+                    'likes': most_active_post.likes,
+                    'dislikes': most_active_post.dislikes
+                }
+
+        return jsonify(most_active_posts)
+
+
+class ExpiredPosts(Resource):
+    @jwt_required
+    def get(self, topic_name):
+        current_user = get_jwt_identity()
+
+        expired_posts = Post.query.filter_by(topic=topic_name, status='Expired').all()
+
+        history_data = []
+
+        for expired_post in expired_posts:
+            history_data.append({
+                'id': expired_post.id,
+                'title': expired_post.title,
+                'topic': expired_post.topic,
+                'timestamp': expired_post.timestamp.strftime('%Y-%m-%dT%H:%M:%S'),
+                'expiration_time': expired_post.expiration_time.strftime('%Y-%m-%dT%H:%M:%S'),
+                'owner_name': expired_post.owner_name,
+                'likes': expired_post.likes,
+                'dislikes': expired_post.dislikes
+            })
+
+        return jsonify({'history_data': history_data})
 
 
 if __name__ == '__main__':
@@ -312,4 +344,7 @@ if __name__ == '__main__':
     api.add_resource(UserInteractions, '/interaction/<int:post_id>')
     api.add_resource(ViewComments, '/comments/<int:post_id>')
     api.add_resource(PostComment, '/comment/<int:post_id>')
+    api.add_resource(MostActivePosts, '/most_active_posts')
+    api.add_resource(ExpiredPosts, '/expired_posts/<string:topic_name>')
+    
     app.run(debug=True)
